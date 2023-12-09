12ab74dc35d50de16b2096ea8f8ed87b075a9861
Giovanna Cunha <110979690+GiovannaSI@users.noreply.github.com>
2023-12-09 14:04:24 -0300

    arrumando erros de importação

--- src/components/Catalogo.jsx
+++ src/components/Catalogo.jsx
@@ -1,11 +1,30 @@
-
 import React from "react";
 import { Link } from "react-router-dom";
 import { FaInstagram, FaTwitter, FaYoutube } from 'react-icons/fa';
 
 import '../components/Catalogo.css';
 
+import weddingCake from "../assets/wedding-cake.png";
+import donut from "../assets/donut.png";
+import chocolate from "../assets/chocolate.png";
+import boloCaseiro from "../assets/boloCaseiro.jpg";
+import boloPote1 from "../assets/boloPote1.jpg";
+import torta from "../assets/torta.jpg";
+import pastaAmericana from "../assets/pastaAmericana.png";
+import boloRosa from "../assets/boloRosa.jpg";
+import tortaChocolate from "../assets/tortaChocolate.jpg";
+import brigadeiroGourmet from "../assets/brigadeiros.jpeg";
+import bemCasado from "../assets/bemCasado.jpg";
+import brownie from "../assets/brownie.jpg";
+import suspiros from "../assets/suspiros.jpg";
+import donuts from "../assets/donuts.jpg";
+import docesFinos from "../assets/docesFinos.jpg";
+import bombons from "../assets/bombons.jpg";
+import bombomAberto from "../assets/bombomAberto.jpg";
+import ovoColher from "../assets/ovoColher.jpg";
+
 const Catalogo = () => {
+
     return (
         <div>
             <nav className="menu-container" >
@@ -22,14 +41,14 @@ const Catalogo = () => {
                         {/* <img src="../src/assets/lupa 3.png" id="lupa" width={30} /> */}
                     </div>
 
-                   
 
+                    
                     <Link id="aulas" to="/lives"> Aulas ao vivo</Link>
                     <Link id="forum" to="/forum"> Fórum</Link>
                     <Link id="plano" to="/plano">ConfeitArt+</Link>
                     <Link id="login" to="/login">Login</Link>
                     <Link id="cadastrar" to="/cadastro">Cadastre-se</Link>
-                  
+
 
                 </div>
 
@@ -44,13 +63,13 @@ const Catalogo = () => {
 
             <div className="categorias-img">
                 <div className="ball-1">
-                    <img className='wedding-cake' src="../src/assets/wedding-cake.png" alt="Descrição significativa da imagem" />
+                    <img className='wedding-cake' src={weddingCake} alt="Descrição significativa da imagem" />
                 </div>
                 <div className="ball-2">
-                    <img className='donut' src="../src/assets/donut.png" alt="Descrição significativa da imagem" />
+                    <img className='donut' src={donut} alt="Descrição significativa da imagem" />
                 </div>
                 <div className="ball-3">
-                    <img className='chocolate' src="../src/assets/chocolate.png" alt="Descrição significativa da imagem" />
+                    <img className='chocolate' src={chocolate} alt="Descrição significativa da imagem" />
                 </div>
             </div>
 
@@ -66,7 +85,7 @@ const Catalogo = () => {
                 <div className="linha1">
 
                     <div className="bolos-caseiros">
-                        <img className="img-boloCaseiro" alt="Bolo Caseiro" src="../src/assets/boloCaseiro.jpg" />
+                        <img className="img-boloCaseiro" alt="Bolo Caseiro" src={boloCaseiro} />
                         <h5 className="categoria-nome" >Confeitaria</h5>
                         <h3 className="cursos">Bolos Caseiros</h3>
                         <Link id="aulaCurso" to="/aulas">
@@ -75,7 +94,7 @@ const Catalogo = () => {
                     </div>
 
                     <div className="bolos-pote">
-                        <img className="img-boloPote" alt="Bolos de Pote" src="../src/assets/boloPote1.jpg" />
+                        <img className="img-boloPote" alt="Bolos de Pote" src={boloPote1} />
                         <h5 className="categoria-nome">Confeitaria</h5>
                         <h3 className="cursos">Bolos de Pote</h3>
                         <Link id="aulaCurso" to="/aulas">
@@ -84,7 +103,7 @@ const Catalogo = () => {
                     </div>
 
                     <div className="confeitaria-basica">
-                        <img className="img-confeitariaBasica" alt="Confeitaria Básica" src="../src/assets/torta.jpg" />
+                        <img className="img-confeitariaBasica" alt="Confeitaria Básica" src={torta} />
                         <h5 className="categoria-nome">Confeitaria</h5>
                         <h3 className="cursos">Confeitaria Básica</h3>
                         <Link id="aulaCurso" to="/aulas">
@@ -98,7 +117,7 @@ const Catalogo = () => {
 
 
                     <div className="pasta-americana">
-                        <img className="img-pastaAmericana" alt="Pasta Americana" src="../src/assets/pastaAmericana.png" />
+                        <img className="img-pastaAmericana" alt="Pasta Americana" src={pastaAmericana} />
                         <h5 className="categoria-nome">Confeitaria</h5>
                         <h3 className="cursos">Pasta Americana</h3>
                         <Link id="aulaCurso" to="/aulas">
@@ -107,7 +126,7 @@ const Catalogo = () => {
                     </div>
 
                     <div className="chantininho">
-                        <img className="img-chantininho" alt="Decoração com Chantininho" src="../src/assets/boloRosa.jpg" />
+                        <img className="img-chantininho" alt="Decoração com Chantininho" src={boloRosa} />
                         <h5 className="categoria-nome">Confeitaria</h5>
                         <h3 className="cursos">Decoração com Chantininho</h3>
                         <Link id="aulaCurso" to="/aulas">
@@ -116,7 +135,7 @@ const Catalogo = () => {
                     </div>
 
                     <div className="tortas">
-                        <img className="img-torta" alt="Torta" src="../src/assets/tortaChocolate.jpg" />
+                        <img className="img-torta" alt="Torta" src={tortaChocolate} />
                         <h5 className="categoria-nome">Confeitaria</h5>
                         <h3 className="cursos">Tortas</h3>
                         <Link id="aulaCurso" to="/aulas">
@@ -134,7 +153,7 @@ const Catalogo = () => {
                 <div className="linha3">
 
                     <div className="brigadeiros-gourmet">
-                        <img className="img-brigadeiros" alt="Brigadeiros Gourmet" src="../src/assets/brigadeiros.jpeg" />
+                        <img className="img-brigadeiros" alt="Brigadeiros Gourmet" src={brigadeiroGourmet} />
                         <h5 className="categoria-nome">Doces</h5>
                         <h3 className="cursos">Brigadeiros Gourmet</h3>
                         <Link id="aulaCurso" to="/aulas">
@@ -143,7 +162,7 @@ const Catalogo = () => {
                     </div>
 
                     <div className="bem-casado">
-                        <img className="img-bemCasado" alt="Bem Casado" src="../src/assets/bemCasado.jpg" />
+                        <img className="img-bemCasado" alt="Bem Casado" src={bemCasado} />
                         <h5 className="categoria-nome">Doces</h5>
                         <h3 className="cursos">Bem Casado</h3>
                         <Link id="aulaCurso" to="/aulas">
@@ -152,7 +171,7 @@ const Catalogo = () => {
                     </div>
 
                     <div className="brownie">
-                        <img className="img-brownie" alt="Brownie" src="../src/assets/brownie.jpg" />
+                        <img className="img-brownie" alt="Brownie" src={brownie} />
                         <h5 className="categoria-nome">Doces</h5>
                         <h3 className="cursos">Brownie</h3>
                         <Link id="aulaCurso" to="/aulas">
@@ -164,7 +183,7 @@ const Catalogo = () => {
 
                 <div className="linha4">
                     <div className="suspiros">
-                        <img className="img-suspiros" alt="Suspiros" src="../src/assets/suspiros.jpg" />
+                        <img className="img-suspiros" alt="Suspiros" src={suspiros} />
                         <h5 className="categoria-nome">Doces</h5>
                         <h3 className="cursos">Suspiros Gourmet</h3>
                         <Link id="aulaCurso" to="/aulas">
@@ -173,7 +192,7 @@ const Catalogo = () => {
                     </div>
 
                     <div className="donuts">
-                        <img className="img-donuts" alt="Donuts" src="../src/assets/donuts.jpg" />
+                        <img className="img-donuts" alt="Donuts" src={donuts} />
                         <h5 className="categoria-nome">Doces</h5>
                         <h3 className="cursos">Donuts</h3>
                         <Link id="aulaCurso" to="/aulas">
@@ -182,7 +201,7 @@ const Catalogo = () => {
                     </div>
 
                     <div className="doces-finos">
-                        <img className="img-docesFinos" alt="Doces Finos" src="../src/assets/docesFinos.jpg" />
+                        <img className="img-docesFinos" alt="Doces Finos" src={docesFinos} />
                         <h5 className="categoria-nome">Doces</h5>
                         <h3 className="cursos">Doces Finos</h3>
                         <Link id="aulaCurso" to="/aulas">
@@ -199,7 +218,7 @@ const Catalogo = () => {
                 <div className="linha5">
 
                     <div className="bombons">
-                        <img className="img-bombons" alt="Bombons" src="../src/assets/bombons.jpg" />
+                        <img className="img-bombons" alt="Bombons" src={bombons} />
                         <h5 className="categoria-nome">Chocolates</h5>
                         <h3 className="cursos">Bombons e Trufas</h3>
                         <Link id="aulaCurso" to="/aulas">
@@ -208,7 +227,7 @@ const Catalogo = () => {
                     </div>
 
                     <div className="bombom-aberto">
-                        <img className="img-bombomAberto" alt="Bombom Aberto" src="../src/assets/bombomAberto.jpg" />
+                        <img className="img-bombomAberto" alt="Bombom Aberto" src={bombomAberto} />
                         <h5 className="categoria-nome">Chocolates</h5>
                         <h3 className="cursos">Bombons Aberto</h3>
                         <Link id="aulaCurso" to="/aulas">
@@ -217,7 +236,7 @@ const Catalogo = () => {
                     </div>
 
                     <div className="ovo-colher">
-                        <img className="img-ovoColher" alt="Ovo de Colher" src="../src/assets/ovoColher.jpg" />
+                        <img className="img-ovoColher" alt="Ovo de Colher" src={ovoColher} />
                         <h5 className="categoria-nome">Chocolates</h5>
                         <h3 className="cursos">Ovos de Colher</h3>
                         <Link id="aulaCurso" to="/aulas">
@@ -272,4 +291,5 @@ const Catalogo = () => {
         </div>
     )
 }
-export default Catalogo
+
+export default Catalogo;
